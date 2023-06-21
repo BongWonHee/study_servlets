5b4404b71ca3369834e74ae26f7b2d5e6afc9919
SSSanga <ksa901125@gmail.com>
2023-06-21 12:38:04 +0900

    B: push JAVA +A_DB, B insert 미해결

---
+++ src/main/java/com/example/study_servlets/Commons/Commons.java
@@ -0,0 +1,12 @@
+package com.example.study_servlets.Commons;
+
+import java.util.UUID;
+
+public class Commons {
+    public String generatUuid() {
+
+        return UUID.randomUUID().toString();
+    
+}
+}
+
