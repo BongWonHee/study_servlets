3bb73115293fbec1884495018f37cd69ad6af9cb
SSSanga <ksa901125@gmail.com>
2023-06-22 09:37:15 +0900

    B: push_어제 과제까지

--- src/main/java/com/example/study_servlets/controlls/ConnectDBServlet.java
+++ src/main/java/com/example/study_servlets/controlls/ConnectDBServlet.java
@@ -1,8 +1,10 @@
 package com.example.study_servlets.controlls;
 
 import java.io.IOException;
-
+import java.io.PrintWriter;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,42 +12,110 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.example.study_servlets.commons.Commons;
+import com.example.study_servlets.daos.CarInforsDao;
+import com.example.study_servlets.daos.FactorysDao;
+
 @WebServlet(urlPatterns = "/connectDBServlet")
 public class ConnectDBServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         try {
-            // - MySQL workbench 실행 : JDBC
-            // - User/password와 접속 IP:port 접속
-            String url = "jdbc:mysql://192.168.224:3306/db_cars";
-            String user = "root";
-            String password = "!yojulab*";
 
-            Connection connection = DriverManager.getConnection(url, user, password); // network 자원사용
-            System.out.println("DB연결 성공\n");
-
-            Statement statement = connection.createStatement();
             String query = "";
 
+            // Client에 html 화면 제공
+            String contents = "<!DOCTYPE html>\r\n" + //
+                    "<html lang=\"en\">\r\n" + //
+                    "\r\n" + //
+                    "<head>\r\n" + //
+                    "    <meta charset=\"UTF-8\">\r\n" + //
+                    "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\r\n" + //
+                    "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + //
+                    "    <title>bootstrap_tables</title>\r\n" + //
+                    "    <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css\">\r\n"
+                    + //
+                    "</head>\r\n" + //
+                    "\r\n" + //
+                    "<body>\r\n" + //
+                    "    <div class=\"container\">\r\n" + //
+                    "        <table class=\"table table-bordered table-hover\">\r\n" + //
+                    "            <thead>\r\n" + //
+                    "                <tr>\r\n" + //
+                    "<thead>\r\n" + //
+                    "                <tr>\r\n" + //
+                    "                    <th>COMPANY_ID</th>\r\n" + //
+                    "                    <th>COMPANY</th>\r\n" + //
+                    "                </tr>\r\n" + //
+                    "            </thead>\r\n" + //
+                    "            <tbody>\r\n";
             // - query Edit
-            statement = connection.createStatement(); // DB자원
-            query = "SELECT * FROM factorys";
-            ResultSet resultSet = statement.executeQuery(query);
             // // FactoryDMLs factoryDMLs = new FactoryDMLs();
             // ResultSet resultSet = factoryDMLs.selectStatements(statement, query); //
             // select문에서 table 형태의 결과값 출력명령어
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("COMPANY_ID") +
-                        resultSet.getString("COMPANY"));
+            // while문이 돌면서 tr의 td 형태로 붙음.
+            FactorysDao factorysDao = new FactorysDao();
+            ArrayList factoryList = new ArrayList();
+            factoryList = factorysDao.selectAll();
+            for (int i = 0; i < factoryList.size(); i = i + 1) {
+                HashMap hashMap = new HashMap();
+                hashMap = (HashMap) factoryList.get(i);
+                contents = contents + "<tr>\r\n" + //
+                        "                    <td>" + hashMap.get("COMPANY_ID") + "</td>\r\n" + //
+                        "                    <td>" + hashMap.get("COMPANY") + "</td>\r\n" + //
+                        "                </tr>\r\n";
             }
+            // ArrayList에서 먼저 나오는건 HashMap..
+            // i=i+1 = i ++
+
+            contents = contents + "                    <th>CAR_NAME</th>\r\n" + //
+                    "                    <th>YEAR</th>\r\n" + //
+                    "                    <th>CAR_INFOR_ID</th>\r\n" + //
+                    "                    <th>COMPANY_ID</th>\r\n" + //
+                    "                </tr>\r\n" + //
+                    "            </thead>\r\n" + //
+                    "            <tbody>\r\n";
+            CarInforsDao carinforsdao = new CarInforsDao();
+            ArrayList carinformation = new ArrayList();
+
+            carinformation = carinforsdao.carInfor();
+            HashMap hashmaps = new HashMap();
+            for (int first = 0; first < carinformation.size(); first = first + 1) {
+                hashmaps = new HashMap();
+                hashmaps = (HashMap) carinformation.get(first);
+                contents = contents + "<tr>\r\n" + //
+                        "                    <td>" + hashmaps.get("CAR_NAME") + "</td>\r\n" + //
+                        "                    <td>" + hashmaps.get("YEAR") + "</td>\r\n" + //
+                        "                    <td>" + hashmaps.get("CAR_INFOR_ID") + "</td>\r\n" + //
+                        "                    <td>" + hashmaps.get("COMPANY_ID") + "</td>\r\n" + //
+                        "                </tr>\r\n";
+
+            }
+
+            contents = contents + "</tbody>\r\n" + //
+                    "        </table>\r\n" + //
+                    "    </div>\r\n" + //
+                    "\r\n" + //
+                    "</body>\r\n" + //
+                    "<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js\"></script>\r\n"
+                    + //
+                    "\r\n" + //
+                    "</html>";
+            response.setContentType("text/html;charset=UTF-8");
+            PrintWriter printWriter = response.getWriter();
+            printWriter.println(contents);
+            printWriter.close();
+
+            // SELECT COUNT(*)
             // query = "select count(*) as CNT from factorys;";
-            // resultSet = factoryDMLs.selectStatements(statement, query); // select문에서
-            // table 형태의 결과값 출력명령어
+            // resultSet = statement.executeQuery(query); // select문에서
+            // // table 형태의 결과값 출력명령어
             // int totalCount = 0;
             // while (resultSet.next()) {
             // System.out.println(resultSet.getInt("CNT"));
-            // totalCount = resultSet.getInt("CNT"); }
+            // totalCount = resultSet.getInt("CNT");
+            // }
 
             // insert into factorys
             // (COMPANY_ID, COMPANY)
