9ed1f06508806cef3d5756630cbc7ceff0371c73
BongWonHee <leeban1004@gmail.com>
2023-06-22 19:54:17 +0900

    add

--- src/main/java/com/example/study_servlets/daos/OptioninforsDao.java
+++ src/main/java/com/example/study_servlets/daos/OptioninforsDao.java
@@ -8,22 +8,22 @@ import java.util.HashMap;
 import com.example.study_servlets.commons.Commons;
 
 public class OptionInforsDao {
-    public int UpdateWithCarInfos(String name) {
-        int count = 0;
-        try {
-            Commons commons = new Commons();
-            Statement statement = commons.getStatement();
-            String query = "update option_infors\n" + //
-                    "set  OPTION_NAME = '" + name + "'\n" + //
-                    "where OPTION_INFOR_ID = 'OI001';\n" + //
-                    "";
-            count = statement.executeUpdate(query);
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-        return count;
-
+public int UpdateWithCarInfos(String name){
+    int count = 0 ;
+    try {
+        Commons commons = new Commons();
+        Statement statement = commons.getStatement();
+        String query="update option_infors\n" + //
+                "set OPTION_NAME = '"+name+"'\n" + //
+                "where OPTION_NAME = OPTION_INFOR_ID = 'OI001';\n" + //
+                "";
+                count = statement.executeUpdate(query);
+    } catch (Exception e) {
+        // TODO: handle exception
     }
+    return count ;
+
+}
 
     public int InsertWithCarInfos(String name) {
         int count = 0;
