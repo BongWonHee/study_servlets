c79eb7aad107164f7d9ae736f0832f2c6aecb022
BongWonHee <leeban1004@gmail.com>
2023-06-22 12:37:36 +0900

    hello

---
+++ src/main/java/com/example/study_servlets/controlls/OptionInforsServlet.java
@@ -0,0 +1,77 @@
+package com.example.study_servlets.controlls;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.example.study_servlets.commons.Commons;
+
+@WebServlet(urlPatterns = "/optionInforsServlet")
+public class OptionInforsServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            Commons commons = new Commons();
+            Statement statement = commons.getStatement();
+            String query = "select *\n" + //
+                    "from option_infors;";
+            ResultSet resultset = statement.executeQuery(query);
+            while (resultset.next()) {
+                System.out.println(resultset.getString("OPTION_INFOR_ID") + "," + resultset.getString("OPTION_NAME"));
+
+            }
+
+            String contents = "<!DOCTYPE html>\r\n" + //
+                    "<html lang=\"en\">\r\n" + //
+                    "\r\n" + //
+                    "<head>\r\n" + //
+                    "    <meta charset=\"UTF-8\">\r\n" + //
+                    "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\r\n" + //
+                    "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n" + //
+                    "    <title>Option_Infos</title>\r\n" + //
+                    "    <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css\">\r\n"
+                    + //
+                    "</head>\r\n" + //
+                    "\r\n" + //
+                    "<body>\r\n" + //
+                    "    <div class=\"container\">\r\n" + //
+                    "        <table class=\"table table-bordered table-hover\">\r\n" + //
+                    "            <thead>\r\n" + //
+                    "                <tr>\r\n" + //
+                    "                    <th>OPTION_INFOR_ID</th>\r\n" + //
+                    "                    <th>OPTION_NAME</th>\r\n" + //
+                    "                </tr>\r\n" + //
+                    "            </thead>\r\n" + //
+                    "            <tbody>\r\n" + //
+                    "                <tr>\r\n" + //
+                    "                    <td>OPTION_INFOR_ID</td>\r\n" + //
+                    "                    <td>OPTION_NAME</td>\r\n" + //
+                    "                </tr>\r\n" + //
+                    "            </tbody>\r\n" + //
+                    "        </table>\r\n" + //
+                    "    </div>\r\n" + //
+                    "\r\n" + //
+                    "</body>\r\n" + //
+                    "<script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js\"></script>\r\n"
+                    + //
+                    "</html>";
+            // 한글이 깨지는 경우에 추가하여 넣어 주어야한다. PrintWriter 위에 넣는다.
+            response.setContentType("text/html;charset=UTF-8");
+
+            PrintWriter printWriter = response.getWriter();
+            printWriter.println(contents);
+            printWriter.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
