4527b2cef59e0b813ce960829dc3803e6d4a9e1d
SSSanga <ksa901125@gmail.com>
2023-06-22 18:06:22 +0900

    B_push 퀘스트 전: Optioninfor_Delete

--- src/main/java/com/example/study_servlets/commons/Commons.java
+++ src/main/java/com/example/study_servlets/commons/Commons.java
@@ -13,12 +13,12 @@ public class Commons {
     public Statement getStatement() {
         // String url = "jdbc:mysql://127.0.0.1:3306/db_cars";
         // String user = "root";
-        String url = "jdbc:mysql://192.168.0.224:3306/db_cars";
-        String user = "yojulab";
-        String password = "!yojulab*";
-        // String url = "jdbc:mysql://192.168.0.46:3306/db_cars";
+        // String url = "jdbc:mysql://192.168.0.224:3306/db_cars";
         // String user = "yojulab";
         // String password = "!yojulab*";
+        String url = "jdbc:mysql://192.168.0.46:3306/db_cars";
+        String user = "yojulab";
+        String password = "!yojulab*";
         
         Statement statement = null;
         try {
