bd6c251f0814dd592ce5368daf62b60aa3f5148b
BongWonHee <leeban1004@gmail.com>
2023-06-26 12:26:35 +0900

    add

--- src/main/java/com/example/study_servlets/controlls/HelloWorldJSPServlet.java
+++ src/main/java/com/example/study_servlets/controlls/HelloWorldJSPServlet.java
@@ -1,33 +1,33 @@
-package com.example.study_servlets;
+package com.example.study_servlets.controlls;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/helloWorldServlet")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet(urlPatterns = "/helloWorldJSPServlet")
+public class HelloWorldJSPServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        System.out.println("HelloWorldServlet - doGet()");
+
         try {
-            String firstName = "WonHee";
-            String secondName = "Bong";
+
             // 클라이언트에게 네트워크를 통해서 전송 하는 것
 
-            response.setContentType("text/html;charset=UTF8");
-            PrintWriter printWriter = response.getWriter();
-            String contents = "Yoju lab !";
-            printWriter.println(contents);
-            printWriter.close();
+            String contents = "Yoju Lab !";
+            request.setAttribute("contents", contents);
+            // 다음 파일 호출 하자.
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/HelloWorldJSP.jsp");
+            requestDispatcher.forward(request, response);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        System.out.println("HelloWorldServlet - doGet() done.");
+
     }
 
 }
